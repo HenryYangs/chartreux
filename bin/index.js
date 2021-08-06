c9874703c451e28b9f6c1cb0e3bf614dd975f70e
HenryYang <sephenry@hotmail.com>
2021-08-06 22:09:03 +0800

    :sparkles: add update command

--- bin/index.js
+++ bin/index.js
@@ -3,5 +3,16 @@
 const commander = require('commander');
 const pkg = require('../package.json');
 
+// --version
 commander.version(pkg.version, '-v', '--version');
+
+// update
+const update = require('../lib/update');
+
+commander.command('update')
+          .description('Check version of chartreux')
+          .action(function() {
+            update();
+          });
+
 commander.parse(process.argv);
